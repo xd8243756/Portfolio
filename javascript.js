ff9c7bbb34ee35283687c77301437d35cf7e2c79
xd8243756 <xd8243756@gmail.com>
2025-10-11 02:11:48 +0800

    新增github第一版

---
+++ javascript.js
@@ -0,0 +1,59 @@
+// const hamBuergerMenu = document.getElementById('hamBurgerMenu');
+// const menu = document.getElementById('menu');
+
+// hamBuergerMenu.addEventListener('click', function(e){
+//     e.stopPropagation();
+//     menu.classList.toggle('active');
+//     hamBuergerMenu.classList.toggle('open');
+
+// })
+
+// menu.addEventListener('click', function(e){
+//     e.stopPropagation();    
+// })
+
+// document.addEventListener('click', function(event){
+//     menu.classList.remove('active');
+//     hamBuergerMenu.classList.remove('open');
+// })
+
+
+
+document.getElementById("defaultOpen").addEventListener("click",function(){
+    openimage("image1",this);
+    console.log(this);
+})
+document.getElementById("open2").addEventListener("click",function(){
+    openimage("image2",this);
+    console.log(this);
+})
+document.getElementById("open3").addEventListener("click",function(){
+    openimage("image3",this);
+    console.log(this);
+})
+document.getElementById("open4").addEventListener("click",function(){
+    openimage("image4",this);
+    console.log(this);
+})
+
+
+function openimage(imageName,element){
+    var i ,tabcontent,tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for( i = 0 ; i < tabcontent.length ; i++)
+    {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for( i = 0 ; i< tablinks.length ; i++){
+        tablinks[i].style.display = "block";
+    }
+
+    document.getElementById(imageName).style.display = "block";
+    
+    element.style.backgroundC0olor = "aqua";
+    
+}
+
+document.getElementById("defaultOpen").click();
