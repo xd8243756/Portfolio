8cd3380d1af94a71761b21faf78b8496c9ecab35
xd8243756 <xd8243756@gmail.com>
2025-10-14 12:35:51 +0800

    新增漢堡選單

--- javascript.js
+++ javascript.js
@@ -19,41 +19,66 @@
 
 
 
-document.getElementById("defaultOpen").addEventListener("click",function(){
-    openimage("image1",this);
+document.getElementById("defaultOpen").addEventListener("click", function () {
+    openimage("image1", this);
     console.log(this);
 })
-document.getElementById("open2").addEventListener("click",function(){
-    openimage("image2",this);
+document.getElementById("open2").addEventListener("click", function () {
+    openimage("image2", this);
     console.log(this);
 })
-document.getElementById("open3").addEventListener("click",function(){
-    openimage("image3",this);
+document.getElementById("open3").addEventListener("click", function () {
+    openimage("image3", this);
     console.log(this);
 })
-document.getElementById("open4").addEventListener("click",function(){
-    openimage("image4",this);
+document.getElementById("open4").addEventListener("click", function () {
+    openimage("image4", this);
     console.log(this);
 })
 
 
-function openimage(imageName,element){
-    var i ,tabcontent,tablinks;
+function openimage(imageName, element) {
+    var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
-    for( i = 0 ; i < tabcontent.length ; i++)
-    {
+    for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
     tablinks = document.getElementsByClassName("tablink");
-    for( i = 0 ; i< tablinks.length ; i++){
+    for (i = 0; i < tablinks.length; i++) {
         tablinks[i].style.display = "block";
     }
 
     document.getElementById(imageName).style.display = "block";
-    
+
     element.style.backgroundC0olor = "aqua";
-    
+
 }
+
+function myfunction() {
+    console.log("A");
+    let x = document.getElementById("nav_List_li")
+    x.classList.toggle("responsive");
+}
+
+const hamBurgerMenu = document.getElementById("HBG");
+const menu = document.querySelector(".nav_List_li")
+if (hamBurgerMenu && menu) {
+    hamBurgerMenu.addEventListener("click", function (e) {
+        e.stopPropagation();
+        menu.classList.toggle("open");
+        console.log(1);
+    });
+
+    menu.addEventListener("click", function (e) {
+        e.stopPropagation();
+        // console.log(2);
+    });
+    document.addEventListener("click", function () {
+        menu.classList.remove("responsive");
+        console.log(3);
+    });
+}
+
 
 document.getElementById("defaultOpen").click();
