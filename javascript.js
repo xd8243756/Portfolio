1fc12fad40836de52980f2ca0a0d7367aafd6052
xd8243756 <xd8243756@gmail.com>
2025-10-15 15:33:31 +0800

    新增作品分類功能

--- javascript.js
+++ javascript.js
@@ -16,8 +16,7 @@
 //     menu.classList.remove('active');
 //     hamBuergerMenu.classList.remove('open');
 // })
-
-
+let slideIndex = 1;
 
 document.getElementById("defaultOpen").addEventListener("click", function () {
     openimage("image1", this);
@@ -35,6 +34,8 @@ document.getElementById("open4").addEventListener("click", function () {
     openimage("image4", this);
     console.log(this);
 })
+document.getElementById("prev").addEventListener("click", goPrev)
+document.getElementById("next").addEventListener("click", goNext)
 
 
 function openimage(imageName, element) {
@@ -51,11 +52,11 @@ function openimage(imageName, element) {
 
     document.getElementById(imageName).style.display = "block";
 
-    element.style.backgroundC0olor = "aqua";
-
 }
 
-function myfunction() {
+
+//處理漢堡選單
+function RWD_Menu() {
 
     let x = document.getElementById("nav_List_li")
     x.classList.toggle("responsive");
@@ -66,8 +67,7 @@ const menu = document.querySelector(".nav_List_li")
 if (hamBurgerMenu && menu) {
     hamBurgerMenu.addEventListener("click", function (e) {
         e.stopPropagation();
-        menu.classList.toggle("open");
-
+        RWD_Menu()
     });
 
     menu.addEventListener("click", function (e) {
@@ -79,6 +79,87 @@ if (hamBurgerMenu && menu) {
 
     });
 }
+
+
+//處理點選箭頭切換圖片
+function goNext() {
+    plusSlides(1);
+}
+function goPrev() {
+    plusSlides(-1);
+}
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function showSlides(n) {
+
+    let slides = document.getElementsByClassName("tabcontent");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+}
+
+
+//處理點選按鈕切換分類
+function filterSelection(c) {
+    let x;
+    x = document.getElementsByClassName("img");
+    console.log(x)
+    if (c == "all") {
+        c = "";
+        document.querySelector(".img_colume_show").style.overflow = "hidden";
+        document.querySelector(".img_colume_show").style.whiteSpace = "normal";
+        document.querySelector(".img_row_show").style.overflow = "hidden";
+        document.querySelector(".img_row_show").style.whiteSpace = "normal";
+        
+    }
+    if(c == "colume"){
+        document.querySelector(".img_colume_show").style.overflow = "auto";
+        document.querySelector(".img_colume_show").style.whiteSpace = "nowrap";
+    }
+    if(c == "row"){
+        document.querySelector(".img_row_show").style.overflow = "auto";
+        document.querySelector(".img_row_show").style.whiteSpace = "nowrap";
+    }
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (let i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+function w3AddClass(element, name) {
+  let i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+
+}
+
+function w3RemoveClass(element, name) {
+  let i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+filterSelection("all");
+showSlides(slideIndex);
+
 // function workslide() {
 //     const box = document.querySelector(".img_colume_show")
 //     box.innerHTML += box.innerHTML;
